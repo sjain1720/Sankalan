9851f60bce6a279fca69566dfb6c8e526678bf42
Navneet Kumar <navneet.18545@sscbs.du.ac.in>
2022-04-02 16:41:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sankalan/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sankalan.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sankalan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
