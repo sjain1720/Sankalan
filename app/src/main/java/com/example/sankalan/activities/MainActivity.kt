eda6350b550e576cd988d6a0bbee39bbe49b1c1b
Navneet Kumar <navneet.18545@sscbs.du.ac.in>
2022-04-16 13:58:49 +0530

    RequiredUpdates

--- app/src/main/java/com/example/sankalan/activities/MainActivity.kt
+++ app/src/main/java/com/example/sankalan/activities/MainActivity.kt
@@ -2,20 +2,15 @@ package com.example.sankalan.activities
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
 import com.example.sankalan.R
 import com.example.sankalan.databinding.ActivityMainBinding
-import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -52,6 +47,10 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    private fun  getUserData(){
+
     }
 
 }
