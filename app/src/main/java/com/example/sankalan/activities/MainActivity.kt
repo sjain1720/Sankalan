055063490cc1d5de650cb4e6e6c3b086d39bff6b
shivpujan12 <shivpujanyadav12@gmail.com>
2022-04-17 12:05:30 +0530

    Half work pending for the Drawerlayout to work

--- app/src/main/java/com/example/sankalan/activities/MainActivity.kt
+++ app/src/main/java/com/example/sankalan/activities/MainActivity.kt
@@ -1,57 +1,21 @@
 package com.example.sankalan.activities
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.fragment.NavHostFragment
 import com.example.sankalan.R
-import com.example.sankalan.databinding.ActivityMainBinding
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-    lateinit var bottom:BottomNavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            startActivity(Intent(this,DrawerActivity::class.java))
+        }
 
-        setSupportActionBar(binding.appBarMain.toolbar)
-        // Bottom Navigation
-        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
-        val navControl = navHost.navController
-        bottom = findViewById(R.id.bottom_navigation)
-        bottom.setupWithNavController(navControl)
-        //drawer layout
-         appBarConfiguration = AppBarConfiguration(navControl.graph,binding.drawerLayout)
-        setupActionBarWithNavController(navControl,binding.drawerLayout)
-        binding.navView.setupWithNavController(navControl)
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 
 }
--- app/src/main/java/com/example/sankalan/activities/SplashScreen.kt
+++ app/src/main/java/com/example/sankalan/activities/SplashScreen.kt
@@ -44,7 +44,8 @@ class SplashScreen : AppCompatActivity() {
                 ignored.printStackTrace()
             }
 
-        }, 5000)
+        }, 2000)
+        // TODO: change the time
 
     }
 
